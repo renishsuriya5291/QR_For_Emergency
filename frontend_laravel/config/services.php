8101b7e2de91da3e865e2cf37085872981107955
renishsuriya5291 <renishsuriya1441@gmail.com>
2024-11-10 22:25:02 +0530

    Msg

--- frontend_laravel/config/services.php
+++ frontend_laravel/config/services.php
@@ -46,7 +46,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
+        'redirect' => 'https://quick-sos.rickmate.in/auth/google/call-back',
     ],
 
     'facebook' => [
